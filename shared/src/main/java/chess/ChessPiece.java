8c3e6d59a01cd1fa3dc0ea5732046a589dfe1952
Morguinha <preston.morgan12@gmail.com>
2024-01-24 16:28:35 -0800

    Worked with TA on pieceMoves, isString, equals and hashtables

---
+++ shared/src/main/java/chess/ChessPiece.java
@@ -0,0 +1,82 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+/**
+ * Represents a single chess piece
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessPiece {
+
+    ChessGame.TeamColor color;
+
+    ChessPiece.PieceType piece;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return color == that.color && piece == that.piece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, piece);
+    }
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.color = pieceColor;
+        this.piece = type;
+    }
+
+    /**
+     * The various different chess piece options
+     */
+    public enum PieceType {
+        KING,
+        QUEEN,
+        BISHOP,
+        KNIGHT,
+        ROOK,
+        PAWN
+    }
+
+    /**
+     * @return Which team this chess piece belongs to
+     */
+    public ChessGame.TeamColor getTeamColor() {
+        return color;
+    }
+
+    /**
+     * @return which type of chess piece this piece is
+     */
+    public PieceType getPieceType() {
+        return piece;
+    }
+
+    /**
+     * Calculates all the positions a chess piece can move to
+     * Does not take into account moves that are illegal due to leaving the king in
+     * danger
+     *
+     * @return Collection of valid moves
+     */
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        if (board.getPiece(myPosition).getPieceType() == PieceType.BISHOP) {
+            return BishopMoves(board, myPosition);
+        }
+        return null;
+    }
+
+    private Collection<ChessMove> BishopMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> Bishop = new ArrayList<>();
+
+        return Bishop;
+    }
+}
