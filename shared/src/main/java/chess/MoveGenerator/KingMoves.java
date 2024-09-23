ac0041d9fd48e3c8e953a0670684a1593e5f90a5
Preston <prston12@byu.edu>
2024-09-22 20:31:36 -0600

    Implemented KnightMoves because its basically the same as king moves

--- shared/src/main/java/chess/MoveGenerator/KingMoves.java
+++ shared/src/main/java/chess/MoveGenerator/KingMoves.java
@@ -9,7 +9,7 @@ import java.util.HashSet;
 
 public class KingMoves implements calculadorXadrez {
 
-    public static HashSet<ChessMove> getMove(ChessBoard board, ChessPosition startPosition) {
+    public static HashSet<ChessMove> getMoves(ChessBoard board, ChessPosition startPosition) {
         int[][] kingOptions = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
         return calculadorXadrez.nonrecursiveMoves(board, kingOptions, startPosition);
     }
--- shared/src/main/java/chess/MoveGenerator/KnightMoves.java
+++ shared/src/main/java/chess/MoveGenerator/KnightMoves.java
@@ -1,4 +1,16 @@
 package chess.MoveGenerator;
 
-public class KnightMoves {
+import chess.ChessPosition;
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+
+import java.util.HashSet;
+
+public class KnightMoves implements calculadorXadrez {
+
+    public static HashSet<ChessMove> getMoves(ChessBoard board, ChessPosition startPosition) {
+        int[][] knightOptions = {{2,-1},{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2}};
+        return calculadorXadrez.nonrecursiveMoves(board, knightOptions, startPosition);
+    }
 }
