ec181f8219c834aa1566ff4910a334f3a97cdb74
Preston <prston12@byu.edu>
2024-09-22 22:17:45 -0600

    Finished for the day, added equals and hash code, not sure why but still no returning of a list of moves, idk tbh.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,10 +1,10 @@
 package chess;
 
-import chess.MoveGenerator.*;
-
-
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
+
+import chess.MoveGenerator.*;
 
 /**
  * Represents a single chess piece
@@ -46,6 +46,19 @@ public class ChessPiece {
      */
     public PieceType getPieceType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
