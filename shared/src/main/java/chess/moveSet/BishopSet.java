7b6658b08f1b6ef5bcc66fb47b5f412266268f86
Preston Morgan <preston.morgan12@gmail.com>
2025-05-06 10:53:42 -0600

    Working on a recursive move set for the bishop, not returning anything yet

--- shared/src/main/java/chess/moveSet/BishopSet.java
+++ shared/src/main/java/chess/moveSet/BishopSet.java
@@ -4,4 +4,11 @@ import chess.*;
 import java.util.HashSet;
 
 public class BishopSet {
+    public static HashSet<ChessMove> generateMoves(ChessBoard board, ChessPosition position) {
+        int currentCol = position.getColumn();
+        int currentRow = position.getRow();
+        int[][] possibleMoves = {{1,-1},{1,1},{-1,1},{-1,-1}};
+
+        return MoveGenerator.recursiveMoves(board, position, possibleMoves, currentRow, currentCol);
+    }
 }
--- shared/src/main/java/chess/moveSet/MoveGenerator.java
+++ shared/src/main/java/chess/moveSet/MoveGenerator.java
@@ -4,4 +4,30 @@ import chess.*;
 import java.util.HashSet;
 
 public class MoveGenerator {
+
+    static HashSet<ChessMove> recursiveMoves(ChessBoard board, ChessPosition position, int[][] possibleMoves, int currentRow, int currentCol) {
+        HashSet<ChessMove> moves = HashSet.newHashSet(30);
+        for (int[] direction : possibleMoves) {
+            boolean blocked = false;
+            int i = 1;
+            while (!blocked) {
+                ChessPosition nextPosition = new ChessPosition(currentRow + direction[1]*i, currentCol + direction[0]*i);
+                if (!MoveGenerator.onBoard(nextPosition)) {
+                    blocked = true;
+                }
+                else if (board.getPiece(nextPosition) == null) {
+                    moves.add(new ChessMove(position, nextPosition, null));
+                }
+            }
+        }
+        return moves;
+    }
+
+    static HashSet<ChessMove> singleMoves() {
+        return null;
+    }
+
+    static boolean onBoard(ChessPosition position) {
+        return (position.getRow() >=1 && position.getColumn() <=8) && (position.getColumn() >=1 && position.getRow() <=8);
+    }
 }
