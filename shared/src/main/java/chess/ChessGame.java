148b6b8f183479adda6d243df2b8b452d5253be4
Preston Morgan <preston.morgan12@gmail.com>
2025-11-20 17:04:03 -0700

    fixed a bug i added the same line of code twice by accident

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -58,6 +58,7 @@ public class ChessGame {
      * @return Set of valid moves for requested piece, or null if no piece at
      * startPosition
      */
+
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         ChessPiece activePiece = board.getPiece(startPosition);
         if (activePiece == null) {
